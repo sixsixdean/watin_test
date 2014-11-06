4b23e3df7a6e3e339657564ca769b9af01eb2b20
sixdean <wangliping89105@126.com>
2014-11-06 15:20:56 +0800

    新加的form

---
+++ src/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbKeyWords = new System.Windows.Forms.TextBox();
+            this.tbWebSite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "搜索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbKeyWords
+            // 
+            this.tbKeyWords.Location = new System.Drawing.Point(108, 53);
+            this.tbKeyWords.Name = "tbKeyWords";
+            this.tbKeyWords.Size = new System.Drawing.Size(116, 21);
+            this.tbKeyWords.TabIndex = 1;
+            // 
+            // tbWebSite
+            // 
+            this.tbWebSite.Location = new System.Drawing.Point(108, 95);
+            this.tbWebSite.Name = "tbWebSite";
+            this.tbWebSite.Size = new System.Drawing.Size(116, 21);
+            this.tbWebSite.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(28, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "关键字:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "网址:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbWebSite);
+            this.Controls.Add(this.tbKeyWords);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbKeyWords;
+        private System.Windows.Forms.TextBox tbWebSite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using WatiN.Core;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string KeyWord = tbKeyWords.Text.Trim();
+            string WebSite = tbWebSite.Text.Trim();
+            MyThreadParameter mtp = new MyThreadParameter(KeyWord, WebSite);
+            Thread newthread = new Thread(new ParameterizedThreadStart(test));
+            newthread.SetApartmentState(ApartmentState.STA);
+            newthread.Start(mtp);
+        }
+        public static void test(object ms)
+        {
+            EventLog log = new EventLog();
+            try
+            {
+                MyThreadParameter mtp = ms as MyThreadParameter;
+                IE ie = new IE("http://www.baidu.com");
+                log.CreateLog("实例化ie");
+                ie.TextField(Find.ById("kw")).Value = mtp.KeyWord;
+                log.CreateLog("输入关键字");
+                ie.Element("su").Click();
+                log.CreateLog("搜索成功");
+                Thread.Sleep(500);
+                List<Span> spanlist = ie.Spans.ToList();
+                List<Div> divlist1 = new List<Div>();
+                Div div2 = ie.Div(Find.ById("container"));
+                 
+                foreach (var s in spanlist)
+                {
+
+                    if (s.InnerHtml != null && s.InnerHtml.Length > 0 && s.InnerHtml.Contains(mtp.WebSite))
+                    {
+                        Div div1 = getParent(s);
+                        divlist1.Add(div1);
+                    }
+                }
+                log.CreateLog("找到" + divlist1.Count + "个关键字");
+                int i = 1;
+                foreach (var d in divlist1)
+                {
+
+                    if (d.Links != null && d.Links.Count > 0)
+                    {
+                        List<Link> linklist = d.Links.ToList();
+                        foreach (var l in linklist)
+                        {
+                            if (l.InnerHtml != null && l.InnerHtml.Length > 0 && l.InnerHtml.Contains(mtp.WebSite))
+                            {
+                                l.Click();
+                                log.CreateLog("第" + i + "次打开网页");
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                log.CreateLog(e.Message.ToString());
+            }
+            finally
+            {
+                log.CreateLog("本次搜索结束!");
+            }
+        }
+
+        public static Div getParent(Element s)
+        {
+            if (s.Parent.GetType() != typeof(Div))
+            {
+                return getParent(s.Parent);
+            }
+            else
+            {
+                return (Div)s.Parent;
+            }
+        }
+    }
+
+    public class MyThreadParameter
+    {
+        private string _keyword;
+        private string _website;
+        public string KeyWord
+        {
+            get
+            {
+                return _keyword;
+            }
+            set
+            {
+                _keyword = value;
+            }
+        }
+        public string WebSite
+        {
+            get
+            {
+                return _website;
+            }
+            set
+            {
+                _website = value;
+            }
+        }
+        public MyThreadParameter(string keyword, string website)
+        {
+            this._keyword = keyword;
+            this._website = website;
+        }
+    }
+
+    public class EventLog
+    {
+        public void CreateLog(string msg)
+        {
+            string dirpath = @"C:\LOG";
+            string filename = System.DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            string filepath = dirpath + @"\"+filename;
+            if (!Directory.Exists(dirpath))
+                Directory.CreateDirectory(dirpath);
+            if (!File.Exists(filepath))
+                File.Create(filepath).Dispose();
+            StreamWriter sw = new StreamWriter(filepath, true);
+            sw.WriteLine(msg + "---" + System.DateTime.Now.ToString());
+            sw.Flush();
+            sw.Close();
+
+        }
+    }
+
+}
